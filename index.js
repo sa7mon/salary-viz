5e8b67c7315f7135e958fa1d1ce23670ef2cba13
Dan Salmon <sa7mon@users.noreply.github.com>
2019-01-22 14:32:29 -0600

    Update index.js

--- index.js
+++ index.js
@@ -35,13 +35,81 @@ function loadData() {
 				}
 			}
 
-        	console.log(csvData);
-        	graph(csvData);
+			console.log(csvData);
+			createTable();
+        	// graph(csvData);
         },
         error: function(data) {
         	console.log("[loadData] Error loading data: ", data);
         }
      });
+}
+
+function createTable() {
+	
+	var headerLabels = {
+		"FIRST_LAST_INITIALS": "Initials",
+		"LONG_DESC": "Description",
+		"COL_DIV_CODE": "Department",
+		"JOB_TITLE": "Job Title",
+		"TENURE_DEC_YR_MO": "Tenure Date",
+		"EMPT_STATE_DATE": "Start Date",
+		"BASE": "Base",
+		"YTD": "YTD",
+	};
+
+	// Add table headers
+	var csvKeys = Object.keys(csvData[0]);
+	csvKeys.forEach(key => {
+		var heading = `<th>${headerLabels[key]}</th>`;
+		$("table thead tr").append(heading);
+	});
+
+	// Add table rows
+	csvData.forEach(csvRow => {
+		var row = `<tr>`;
+		csvKeys.forEach(key => {
+			row += `<td>${csvRow[key]}</td>`;
+		});
+		row += `</tr>`;
+		$("table tbody").append(row);
+	});
+
+	// Initialize DataTable
+	// $('#salary-table').DataTable();
+
+	/**
+	 * Add column filtering 
+	 * https://datatables.net/extensions/fixedheader/examples/options/columnFiltering.html
+	 */
+
+	// Setup - add a text input to each footer cell
+    // $('#salary-table thead tr').clone(true).appendTo( '#salary-table thead' );
+    // $('#salary-table thead tr:eq(1) th').each( function (i) {
+    //     var title = $(this).text();
+    //     $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+ 
+    //     $( 'input', this ).on( 'keyup change', function () {
+    //         if ( table.column(i).search() !== this.value ) {
+    //             table
+    //                 .column(i)
+    //                 .search( this.value )
+    //                 .draw();
+    //         }
+    //     } );
+    // } );
+ 
+    // var table = $('#salary-table').DataTable( {
+    //     orderCellsTop: true,
+	// 	   fixedHeader: true,
+	// 	responsive: true
+	// });
+
+	var table = $('#salary-table').DataTable( {
+		responsive: true,
+    } );
+ 
+    new $.fn.dataTable.FixedHeader( table );
 }
 
 
