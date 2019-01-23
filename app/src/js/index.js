78a303d1f9a442dba60def7724d5910290ca2102
Dan Salmon <daniel.salmon@mnsu.edu>
2019-01-23 15:34:04 +0000

    Feed the data directly to DataTables so we can render it

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -38,58 +38,41 @@ function loadData(handleData) {
 
 function createTable(csvData) {
 	
-	var headerLabels = {
-		"FIRST_LAST_INITIALS": "Initials",
-		"LONG_DESC": "Description",
-		"COL_DIV_CODE": "Department",
-		"JOB_TITLE": "Job Title",
-		"TENURE_DEC_YR_MO": "Tenure Date",
-		"EMPT_STATE_DATE": "Start Date",
-		"BASE": "Base",
-		"YTD": "YTD",
-	};
-
-	// Add table headers
-	var csvKeys = Object.keys(csvData[0]);
-	csvKeys.forEach(key => {
-		var heading = `<th>${headerLabels[key]}</th>`;
-		$("table thead tr").append(heading);
-	});
-
-	// Add table rows
-	csvData.forEach(csvRow => {
-		var row = `<tr>`;
-		csvKeys.forEach(key => {
-			row += `<td>${csvRow[key]}</td>`;
-		});
-		row += `</tr>`;
-		$("table tbody").append(row);
-	});
-
-	
-	/**
-	 * Add column filtering 
-	 * https://datatables.net/extensions/fixedheader/examples/options/columnFiltering.html
-	 */
-	$('#salary-table thead tr').clone(true).appendTo( '#salary-table thead' );
-    $('#salary-table thead tr:eq(1) th').each( function (i) {
-		var title = $(this).text();
-        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+	// /**
+	//  * Add column filtering 
+	//  * https://datatables.net/extensions/fixedheader/examples/options/columnFiltering.html
+	//  */
+	// $('#salary-table thead tr').clone(true).appendTo( '#salary-table thead' );
+ //   $('#salary-table thead tr:eq(1) th').each( function (i) {
+	// 	var title = $(this).text();
+ //       $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
 		
-        $( 'input', this ).on( 'keyup change', function () {
-			if ( table.column(i).search() !== this.value ) {
-				table
-				.column(i)
-				.search( this.value )
-				.draw();
-            }
-        } );
-    } );
+ //       $( 'input', this ).on( 'keyup change', function () {
+	// 		if ( table.column(i).search() !== this.value ) {
+	// 			table
+	// 			.column(i)
+	// 			.search( this.value )
+	// 			.draw();
+ //           }
+ //       } );
+ //   } );
+	
 	
 	// Initialize DataTable
     var table = $('#salary-table').DataTable( {
         orderCellsTop: true,
 		fixedHeader: true,
+		data: csvData,
+		"columns": [
+			{ "data": "FIRST_LAST_INITIALS", "title": "Initials"},	
+			{ "data": "LONG_DESC", "title": "Description"},
+			{ "data": "COL_DIV_CODE", "title": "Department"},
+			{ "data": "JOB_TITLE", "title": "Job Title"},
+			{ "data": "TENURE_DEC_YR_MO", "title": "Tenure Date"},
+			{ "data": "EMPT_STATE_DATE", "title": "Start Date"},
+			{ "data": "BASE", "title": "Base"},
+			{ "data": "YTD", "title": "YTD"},
+		],
 		//responsive: true
 	});
 }
