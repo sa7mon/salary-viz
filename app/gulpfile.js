675f2f866d3df350df38067d9bcded3c7a8b1f28
Dan Salmon <daniel.salmon@mnsu.edu>
2019-01-23 22:59:11 +0000

    Move datatable libs to npm

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -19,6 +19,24 @@ gulp.task('app-js', function(cb) {
     ], cb);
 });
 
+gulp.task('vendor-js', function(cb) {
+    pump([
+        gulp.src([
+            'node_modules/datatables.net-bs4/js/dataTables.bootstrap4.min.js',
+            'node_modules/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js',
+            'node_modules/datatables.net-responsive/js/dataTables.responsive.min.js',
+            'node_modules/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js'
+        ]),
+        concat('vendor.min.js'),
+        gulp.dest('dist/js/'),
+        babel({
+            presets: ['env']
+        }),
+        uglify(),
+        gulp.dest('dist/js/')
+    ], cb);
+});
+
 gulp.task('app-css', function(cb) {
     pump([
         gulp.src(['src/css/*.css']),
