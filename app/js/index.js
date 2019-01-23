c5714041c0a5b414363b3db9fc660590d4ef64c4
Dan Salmon <dan.sa7mon@gmail.com>
2019-01-22 18:28:16 -0600

    Fix async code and get search row working

--- app/js/index.js
+++ app/js/index.js
@@ -11,41 +11,32 @@ var csvData;
 
 // $(document).ready(init());
 
-function init() {
-	loadData();
-}
+// function init() {
+// 	loadData();
+// }
 
 
-function loadData() {
+function loadData(handleData) {
 	$.ajax({
         type: "GET",
         url: "data/data_sampled.csv",
         dataType: "text",
         success: function(data) {
-        	csvData = $.csv.toObjects(data);
-        	// csvData.map(function(d) {
-        	// 	d["BASE"] = d["BASE"].replace("$", "");
-        	// })
-        	// var csvKeys = Object.keys(csvData[0]);
-        	// console.log(csvKeys);
-
+			csvData = $.csv.toObjects(data);
 			for (var i = csvData.length - 1; i >= 0; i--) {
 				if (csvData[i]["BASE"] !== undefined) {
 					csvData[i]["BASE"] = Number(csvData[i]["BASE"].replace("$", "").replace(",",""));
 				}
 			}
-
-			console.log(csvData);
-			createTable();
-        	// graph(csvData);
+			handleData(csvData);
         },
         error: function(data) {
         	console.log("[loadData] Error loading data: ", data);
         }
-     });
+	 });
 }
 
-function createTable() {
+function createTable(csvData) {
 	
 	var headerLabels = {
 		"FIRST_LAST_INITIALS": "Initials",
@@ -75,41 +66,32 @@ function createTable() {
 		$("table tbody").append(row);
 	});
 
-	// Initialize DataTable
-	// $('#salary-table').DataTable();
-
+	
 	/**
 	 * Add column filtering 
 	 * https://datatables.net/extensions/fixedheader/examples/options/columnFiltering.html
 	 */
-
-	// Setup - add a text input to each footer cell
-    // $('#salary-table thead tr').clone(true).appendTo( '#salary-table thead' );
-    // $('#salary-table thead tr:eq(1) th').each( function (i) {
-    //     var title = $(this).text();
-    //     $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
- 
-    //     $( 'input', this ).on( 'keyup change', function () {
-    //         if ( table.column(i).search() !== this.value ) {
-    //             table
-    //                 .column(i)
-    //                 .search( this.value )
-    //                 .draw();
-    //         }
-    //     } );
-    // } );
- 
-    // var table = $('#salary-table').DataTable( {
-    //     orderCellsTop: true,
-	// 	   fixedHeader: true,
-	// 	responsive: true
-	// });
-
-	var table = $('#salary-table').DataTable( {
-		responsive: false,
+	$('#salary-table thead tr').clone(true).appendTo( '#salary-table thead' );
+    $('#salary-table thead tr:eq(1) th').each( function (i) {
+		var title = $(this).text();
+        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+		
+        $( 'input', this ).on( 'keyup change', function () {
+			if ( table.column(i).search() !== this.value ) {
+				table
+				.column(i)
+				.search( this.value )
+				.draw();
+            }
+        } );
     } );
- 
-    new $.fn.dataTable.FixedHeader( table );
+	
+	// Initialize DataTable
+    var table = $('#salary-table').DataTable( {
+        orderCellsTop: true,
+		fixedHeader: true,
+		//responsive: true
+	});
 }
 
 
